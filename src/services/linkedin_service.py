b8dc8f244733e23236778a943280a7198aebc3de
Teklu <tekluabayneh@gmail.com>
2025-12-25 03:49:54 +0900

    Update LinkedIn service logic

--- src/services/linkedin_service.py
+++ src/services/linkedin_service.py
@@ -2,3 +2,23 @@
 def Post_Linkedin(message):
     return f"response from {message}"
 
+{
+ "author": "urn:li:organization:5515715",
+ "commentary": "test article post",
+ "visibility": "PUBLIC",
+ "distribution": {
+   "feedDistribution": "MAIN_FEED",
+   "targetEntities": [],
+   "thirdPartyDistributionChannels": []
+ },
+ "content": {
+     "article": {
+         "source": "https://lnkd.in/eabXpqi",
+         "thumbnail": "urn:li:image:C49klciosC89",
+         "title": "prod test title two",
+         "description": "test description"
+     }
+ },
+ "lifecycleState": "PUBLISHED",
+ "isReshareDisabledByAuthor": false
+}
