ced23d074622b125a6237d66317d51c7884f8f34
Teklu <tekluabayneh@gmail.com>
2025-12-27 19:22:51 +0900

    Update post_worker.py to handle LinkedIn posting flow

--- src/workers/post_worker.py
+++ src/workers/post_worker.py
@@ -1,13 +1,15 @@
-# from src.services.facebook_service import Post_FaceBook
-# from src.services.reddit_service import Post_Reddit
-# from src.services.linkedin_service import Post_Linkedin
-# from src.services.x_service import Post_x
-from src.services.getAccessToken import GetAccessToken
+from src.services.facebook_service import Post_FaceBook
+from src.services.reddit_service import Post_Reddit
+from src.services.linkedin_service import Post_Linkedin
+from src.services.x_service import Post_x
+from src.services.getlikedinId import getUserId 
+# from src.services.getAccessToken import GetAccessToken
 
 
 def post_everywhere(message):
     # print(Post_FaceBook(message))
     # print(Post_Linkedin(message))
-    # print(Post_Reddit(message))
+    # # print(Post_Reddit(message))
     # print(Post_x(message))
-    print(GetAccessToken())
+    # print(GetAccessToken())
+    print(getUserId)
